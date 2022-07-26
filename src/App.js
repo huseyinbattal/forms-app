fb3588bc0aea16cb09bcca8b1a73c4f575717694
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-07-26 13:38:33 +0300

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-screen flex items-center justify-center bg-gray-400">
+       <div className="w-full max-w-xs">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label
+            htmlFor="userName"
+            className="block text-gray-800 text-sm font-bold mb-4"
+          >
+            Kullanıcı adı <span className="text-red-500">*</span>
+          </label>
+          <input
+            name="userName"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 focus:outline-none focus:shadow-violet-600"
+            />
+            <p className="text-red-500 text-xs italic mt-3">Bu alan zorunludur</p>
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="password"
+            className="block text-gray-800 text-sm font-bold mb-4"
+          >
+            Parola <span className="text-red-500">*</span>
+          </label>
+          <input
+            name="password"
+            type="password"
+            placeholder="**********"
+            className="shadow appearance-none border rounded w-full py-2 px-3
+             text-gray-800 focus:outline-none focus:shadow-violet-600"
+            />
+            <p className="mt-3 text-red-500 text-xs italic">Bu alan zorunludur</p>
+            
+        </div>
+        <div className="flex items-center justify-center">
+          <button 
+            type="button"
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold text-sm 
+          cursor-pointer focus:outline-none focus:shadow-blue-500 py-2 px-4 rounded w-full"
+          >
+            Giriş
+          </button>
+        </div>
+      </form>
     </div>
+   </div>
   );
 }
 
