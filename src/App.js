cc19da1fb03b9976c34ecbffdfdf1b92b60df9ea
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-07-26 18:46:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import { useState } from "react";
 function App() {
+  const [formState, setFormState] = useState([
+    {
+      field: "userName",
+      hasError: false,
+      label:"User Name"
+    },
+
+    {
+      field: "userPassword",
+      hasError: false,
+      label:"Password"
+    },
+  ]);
   const [formModel, setFormModel] = useState({
     userName: "",
     userPassword: "",
@@ -20,31 +33,56 @@ function App() {
   const formValidation = (e) => {
     e.preventDefault();
     const obj = {
-      userName:formModel.userName.trim()==="",
-      userPassword:formModel.userPassword.trim()==="",
-    }
-    setFormErrorModel(obj)
+      userName: formModel.userName.trim() === "",
+      userPassword: formModel.userPassword.trim() === "",
+    };
+    setFormErrorModel(obj);
     if (Object.values(obj).includes(true)) {
-      alert("Error")
-      return
+      alert("Error");
+      return;
     }
-   sendFormRequest()
+    sendFormRequest();
   };
 
   const sendFormRequest = () => {
     setTimeout(() => {
-      alert( "Kullanıcı girişi başarılı")
+      alert("Kullanıcı girişi başarılı");
     }, 2000);
-  }
+  };
 
   return (
     <div className="w-full h-screen flex items-center justify-center bg-gray-400">
       <div className="w-full max-w-xs">
         <form
           className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-          onSubmit={formValidation}
-        >
-          <div className="mb-4">
+          onSubmit={formValidation}>
+          {formState.map((item) => {
+            return (
+              <div className="mb-4">
+              <label
+                htmlFor="userName"
+                className="block text-gray-800 text-sm font-bold mb-4"
+              >
+                {item.label} <span className="text-red-500">*</span>
+              </label>
+              <input
+                onChange={(e) => {
+                  onChange(e.target.value, "userName");
+                }}
+                value={formModel.userName}
+                name="userName"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 focus:outline-none focus:shadow-violet-600"
+              />
+              {formErrorModel.userName && (
+                <p className="text-red-500 text-xs italic mt-3">
+                  Bu alan zorunludur{" "}
+                </p>
+              )}
+            </div>
+            )
+          })}
+        
+          {/* <div className="mb-4">
             <label
               htmlFor="userName"
               className="block text-gray-800 text-sm font-bold mb-4"
@@ -88,7 +126,7 @@ function App() {
                 Bu alan zorunludur{" "}
               </p>
             )}
-          </div>
+          </div> */}
           <div className="flex items-center justify-center">
             <button
               type="submit"
